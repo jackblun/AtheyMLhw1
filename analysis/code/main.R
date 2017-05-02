0aeab2332afe3f97d2b5ecafc6d88f8abbc1a144
Jack Blundell <jackblun@gmail.com>
2017-05-02 12:27:21 +0100

    Initalized repository / directory structure

---
+++ analysis/code/main.R
@@ -0,0 +1,28 @@
+############################################################
+### ECON 293 Homework 1
+
+# Luis Armona and Jack Blundell
+# Stanford University
+# Spring 2017
+
+############################################################
+
+# set your working directory
+
+setwd("C:\\Users\\Jack\\Dropbox\\Documents\\Stanford\\Courses\\Econometrics 3\\Assignments\\Assignment 4")
+
+# clear things in RStudio
+
+rm(list = ls())
+
+
+# Call packages
+
+#library(stats)
+#library(MASS)
+
+# set seed
+set.seed(12345)
+
+############################################################
+
