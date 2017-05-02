ae77b2b05f5411df6bcd7a8a763b2640b8c01f3c
Jack Blundell <jackblun@gmail.com>
2017-05-02 15:17:33 +0100

    Exploratory data analysis

--- analysis/code/main.R
+++ analysis/code/main.R
@@ -9,7 +9,7 @@
 
 # set your working directory
 
-setwd("C:\\Users\\Jack\\Documents\\Git\\Athey ML homework 1\\AtheyMLhw1")
+setwd("C:\\Users\\Jack\\Documents\\Git\\Athey ML homework 1\\AtheyMLhw1") # Jack
 
 # clear things in RStudio
 
@@ -29,3 +29,25 @@ set.seed(12345)
 # Load data
 
 char <- read.csv("analysis\\input\\charitable_withdummyvariables.csv")
+attach(char) # attach so don't have to call each time
+
+
+### Exploratory analysis
+
+dim(char) # 50,083 obs, 63 vars
+names(char)
+head(char) # Look at first few entries of each var
+
+# Treatment
+
+summary(treatment) # This var seems to pool treatments
+mean(treatment) # 67% treated
+
+
+# Giving
+
+summary(out_amountgive) # amount given. Highly skewed
+hist(out_amountgive)
+hist(out_amountgive[out_amountgive<=10])
+
+### 
